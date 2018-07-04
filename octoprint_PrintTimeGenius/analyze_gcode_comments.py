85da91be960195aaa3ed2f1fd903837fbcc5755a
Eyal Soha <eyalsoha@gmail.com>
2018-07-04 22:31:44 +0300

    Cura330 support for filament length and total time
    Working on https://github.com/eyal0/OctoPrint-PrintTimeGenius/issues/6

--- octoprint_PrintTimeGenius/analyze_gcode_comments.py
+++ octoprint_PrintTimeGenius/analyze_gcode_comments.py
@@ -11,24 +11,27 @@ def get_analysis_from_gcode(machinecode_path):
   The analysis structure should look like this:
   http://docs.octoprint.org/en/master/modules/filemanager.html#octoprint.filemanager.analysis.GcodeAnalysisQueue
   (There is a bug in the documentation, estimatedPrintTime should be in seconds.)
-  Return None if there is no analysis information in the file.
+  Return '{}' if there is no analysis information in the file.
   """
-  filament_length = None
-  filament_volume = None
-  printing_seconds = None
+  dd = lambda: defaultdict(dd)
+  analysis = dd()
   with open(machinecode_path) as gcode_lines:
     for gcode_line in gcode_lines:
       if not gcode_line[0].startswith(";"):
         continue # This saves a lot of time
+
+      # Match a slic3r PE filament line
       m = re.match('\s*;\s*filament used\s*=\s*([0-9.]+)\s*mm\s*\(([0-9.]+)cm3\)\s*', gcode_line)
       if m:
-        filament_length = float(m.group(1))
-        filament_volume = float(m.group(2))
+        analysis['filament']['tool0']['length'] = float(m.group(1))
+        analysis['filament']['tool0']['volume'] = float(m.group(2))
+
+      # Match a Slic3r PE print time estimate
       m = re.match('\s*;\s*estimated printing time\s*=\s(.*)\s*', gcode_line)
       if m:
         time_text = m.group(1)
         # Now extract the days, hours, minutes, and seconds
-        printing_seconds = 0
+        analysis['estimatedPrintTime'] = 0
         for time_part in time_text.split(' '):
           for unit in [("h", 60*60),
                        ("m", 60),
@@ -36,20 +39,20 @@ def get_analysis_from_gcode(machinecode_path):
                        ("d", 24*60*60)]:
             m = re.match('\s*([0-9.]+)' + re.escape(unit[0]), time_part)
             if m:
-              printing_seconds += float(m.group(1)) * unit[1]
-  # Now build up the analysis struct
-  dd = lambda: defaultdict(dd)
-  analysis = dd()
-  if printing_seconds is not None or filament_length is not None or filament_volume is not None:
-    if printing_seconds is not None:
-      analysis['estimatedPrintTime'] = printing_seconds
-    if filament_length is not None:
-      analysis['filament']['tool0']['length'] = filament_length
-    if filament_volume is not None:
-      analysis['filament']['tool0']['volume'] = filament_volume
-    # Convert it to a regular string.
-    return json.loads(json.dumps(analysis))
-  return None
+              analysis['estimatedPrintTime'] += float(m.group(1)) * unit[1]
+
+      # Match Cura330 time estimate
+      m = re.match('\s*;\s*TIME_ELAPSED\s*:\s*([0-9.]+)\s*', gcode_line)
+      if m:
+        time_text = m.group(1)
+        analysis['estimatedPrintTime'] = float(time_text)
+
+      # Match Cura330 filament used
+      m = re.match('\s*;\s*Filament used\s*:\s*([0-9.]+)\s*m\s*', gcode_line)
+      if m:
+        filament_meters_text = m.group(1)
+        analysis['filament']['tool0']['length'] = float(filament_meters_text) * 1000
+  return json.loads(json.dumps(analysis))
 
 if __name__ == "__main__":
   print(get_analysis_from_gcode(sys.argv[1]))
