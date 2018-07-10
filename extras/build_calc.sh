3598a806499885f0e742652b4d749deec28b131d
Eyal Soha <eyalsoha@gmail.com>
2018-07-10 13:11:41 +0300

    update build_calc script

--- extras/build_calc.sh
+++ extras/build_calc.sh
@@ -2,7 +2,9 @@
 
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
+pushd "${SCRIPTPATH}"
 git submodule update --remote --recursive
+popd
 pushd "${SCRIPTPATH}/Marlin/Marlin"
 make -f Makefile.calc
 cp -f "calc" "${SCRIPTPATH}/../octoprint_PrintTimeGenius/marlin-calc"
