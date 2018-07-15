0945d926c3a608725701c9bd384c1127fc29b490
Eyal Soha <eyalsoha@gmail.com>
2018-07-15 16:30:50 +0300

    Add plugin requirement for OctoPrint>=1.3.9rc1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/eyal0/OctoPrint-PrintTimeGenius"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["OctoPrint>=1.3.9rc1"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
