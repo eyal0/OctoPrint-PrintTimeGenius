c127d69f2da796480a45295328edc65301d94d3e
Eyal Soha <eyalsoha@gmail.com>
2018-07-22 14:29:09 +0300

    Increment to 1.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_PrintTimeGenius"
 plugin_name = "OctoPrint-PrintTimeGenius"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.8"
+plugin_version = "1.0.9"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
